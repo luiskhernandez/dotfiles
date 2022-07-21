ae57cb9ed217ab1295a0d88c05ce934cc0b5bdbb
Luis Hernandez <luis.macias@MacBook-Pro-de-Luis.local>
2022-07-21 12:30:50 -0500

    NVIM. Basic lua configuration.

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,16 @@
+-- Neovim API aliases
+
+local map = vim.api.nvim_set_keymap
+local cmd = vim.cmd
+local default_opts = {noremap = true, silent = true}
+
+-- General
+
+map('i','jk','<ESC>', default_opts)
+
+-- Disable arrow keys
+
+map('','<up>', '<nop>', {noremap = true })
+map('','<down>', '<nop>', {noremap = true })
+map('','<left>', '<nop>', {noremap = true })
+map('','<right>', '<nop>', {noremap = true })
---
+++ nvim/old_init.vim
@@ -0,0 +1,6 @@
+set number relativenumber
+set mouse=a
+syntax enable
+set showcmd
+set encoding=utf-8
+set showmatch
