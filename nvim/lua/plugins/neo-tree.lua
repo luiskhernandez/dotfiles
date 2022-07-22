385dfe9843dab60f9a6b2c524fcc3c7b0d7059c2
Luis C Hernández <luis.macias@koombea.com>
2022-07-22 12:10:27 -0500

    NVIM add neo-tree initial setup.

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,8 @@
+require("neo-tree").setup({
+  close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+  popup_border_style = "rounded",
+  filesystem = {
+    follow_current_file = true,
+  }
+)}
+
