572d863969c090258fda5ca1f42b3882b94224e4
Luis C Hernández <luis.macias@koombea.com>
2022-07-27 07:45:16 -0500

    NVIM add toggleterm.

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -88,6 +88,9 @@ return require('packer').startup(function(use)
   use 'JoosepAlviste/nvim-ts-context-commentstring'
   -- Git
   use "lewis6991/gitsigns.nvim"  --
+  -- ToggleTerm
+  use "akinsho/toggleterm.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
